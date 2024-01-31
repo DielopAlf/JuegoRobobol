151f908992da0f819953717689334fe1b21f00a0
DielopAlf <114227973+DielopAlf@users.noreply.github.com>
2024-01-31 17:56:10 +0000

    UI en proceso
    Fuentes
    Assets de botones
    Scripts

---
+++ Robolol/Assets/_UI/SCRIPTS  UI/ButtonController.cs
@@ -0,0 +1,81 @@
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class ButtonController : MonoBehaviour
+{
+    public GameObject optionsMenu;
+    public Animator optionsMenuAnimator;
+    private string Menu_Opening, Menu_Closing;
+    private bool isOptionMenuOpen = false;
+    public Button playButton, optionsButton, exitButton;
+        public GameObject creditsCanvas;
+
+    void Start()
+    {
+        optionsMenu.SetActive(false);
+    }
+
+    public void StartGame()
+    {
+        SceneManager.LoadScene("ObstaclesScene");
+    }
+
+    public void CloseGame()
+    {
+        Debug.Log("Se ha cerrado el juego");
+        Application.Quit();
+    }
+
+    public void OpenOptionsMenu()
+    {
+        optionsMenu.SetActive(true);
+        optionsMenuAnimator.Play("Menu_Opening");
+        isOptionMenuOpen = true;
+    }
+
+    public void CloseOptionsMenuAnimation()
+    {
+        optionsMenuAnimator.Play("Menu_Closing");
+        optionsMenuAnimator.SetBool("Closed", true);
+        Invoke("CloseOptionsMenu", 0.10f);
+    }
+
+    public void CloseOptionsMenu()
+    {
+        optionsMenu.SetActive(false);
+        isOptionMenuOpen = false;
+    }
+    public void Credits()
+    {
+        creditsCanvas.SetActive(true);
+    }
+
+    public void BackMainMenu()
+    {
+        creditsCanvas.SetActive(false);
+    }
+    public void Update()
+    {
+        if (isOptionMenuOpen == true)
+        {
+            playButton.interactable = false;
+            optionsButton.interactable = false;
+            exitButton.interactable = false;
+        }
+        else
+        {
+            playButton.interactable = true;
+            optionsButton.interactable = true;
+            exitButton.interactable = true;
+        }
+    }
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+
+}
---
+++ Robolol/Assets/_UI/SCRIPTS  UI/Menupausa.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class Menupausa : MonoBehaviour
+
+{
+    [SerializeField]
+    GameObject pauseContainer;
+
+    private void Update()
+    {
+        if (Input.GetKeyUp(KeyCode.Escape))
+        {
+            if (pauseContainer.activeInHierarchy == true)
+            {
+                pauseContainer.SetActive(false);
+            }
+            else
+            {
+                pauseContainer.SetActive(true);
+            }
+        }
+    }
+    public void Reset()
+    {
+        SceneManager.LoadScene("GameplayScene");
+    }
+    public void Resume()
+    {
+        if (pauseContainer.activeInHierarchy == true)
+        {
+            pauseContainer.SetActive(false);
+        }
+        else
+        {
+            pauseContainer.SetActive(true);
+        }
+    }
+    public void ReturnToMainMenu()
+    {
+        SceneManager.LoadScene("MainmenuScene");
+    }
+}
