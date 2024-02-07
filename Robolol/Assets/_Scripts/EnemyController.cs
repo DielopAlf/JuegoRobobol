d49bc5e679f1baf5cd5266b197509459a073ff1d
TomatitoFeliz <odassorodztomas@gmail.com>
2024-02-07 13:57:32 +0000

    JumpScriptEmpezado

--- Robolol/Assets/_Scripts/EnemyController.cs
+++ Robolol/Assets/_Scripts/EnemyController.cs
@@ -7,10 +7,26 @@ public class EnemyController : MonoBehaviour
 {
     [SerializeField]
     Transform player;
+    [SerializeField]
+    float distanceJump;
+    Vector3 playerLastPosition;
 
     private void Update()
     {
         NavMeshAgent agent = GetComponent<NavMeshAgent>();
-        agent.destination = player.position;
+        if (Vector3.Distance(gameObject.transform.position, player.position) <= distanceJump)
+        {
+            Jump();
+        }
+        else
+        {
+            agent.destination = player.position;
+        }
+    }
+    void Jump()
+    {
+        playerLastPosition = player.position;
+        //gameObject.transform.Translate(new Vector3 (playerLastPosition.x, Mathf.Sin(playerLastPosition.y), playerLastPosition.z) * Time.deltaTime);
+        Debug.Log("Has jumped");
     }
 }
