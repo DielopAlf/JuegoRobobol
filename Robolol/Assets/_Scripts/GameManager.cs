ad75d9e43d0122857edd7c90bb0bdcac67b56bce
TomatitoFeliz <odassorodztomas@gmail.com>
2024-02-21 13:15:24 +0000

    Conjunto

--- Robolol/Assets/_Scripts/GameManager.cs
+++ Robolol/Assets/_Scripts/GameManager.cs
@@ -1,9 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
+    public GameObject defeatScreen;
+    public GameObject victoryScreen;
     private void Start()
     {
         Time.timeScale = 1.0f;
@@ -12,8 +15,9 @@ public class GameManager : MonoBehaviour
     {
         if (GameObject.FindGameObjectWithTag("Player") == null)
         {
-            Debug.Log("Partida Terminada");
-            Time.timeScale = 0f;
+            Time.timeScale = 0;
+            defeatScreen.SetActive(true);
+            Debug.Log("Game Over - Player Died");
         }
         if (GameObject.FindGameObjectWithTag("Enemy") != null)
         {
@@ -21,8 +25,23 @@ public class GameManager : MonoBehaviour
         }
         if (GameObject.FindGameObjectWithTag("Enemy") == null)
         {
-            Debug.Log("Partida Terminada");
-            Time.timeScale = 0f;
+            Time.timeScale = 0;
+            victoryScreen.SetActive(true);
+            Debug.Log("Victory - All enemies defeated");
         }
+    }
+    public void RestartGame()
+    {
+        Time.timeScale = 1;
+        defeatScreen.SetActive(false);
+        victoryScreen.SetActive(false);
+
+        SceneManager.LoadScene("ESCENA DE PRUEBA");
+    }
+
+    public void LoadMainMenu()
+    {
+        Time.timeScale = 1;
+        SceneManager.LoadScene("Main menu");
     }
 }
